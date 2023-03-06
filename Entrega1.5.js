a06f17891aa8740524cfd956f89775ec906acb15
paterbcn <agustinpaternoster@gmail.com>
2023-03-06 13:04:14 +0100

    n1 ejercicio 3

--- Entrega1.5.js
+++ Entrega1.5.js
@@ -11,6 +11,18 @@ fs.appendFile("./Files/testN1Ej1.txt","Prueba N1 ejercio 1",(err)=>{
 fs.writeFile("./Files/testN1Ej1.txt","modificacion del texto generado anteriormente",(err)=>{
     if (err){throw err};console.log("archivo escrito correctamente")})
 
+// N1 ejercicio 2
+//Crea una altra funció que mostri per consola el contingut del fitxer de l'exercici anterior.
+
+fs.readFile("./Files/testN1Ej1.txt","utf-8",(err,data)=>{
+if (err){throw err};console.log(data)  })
+
+// N1 ejercicio 3
+// Crea una funció que comprimeixi el fitxer del nivell 1.
+
+
+
+
 
 
 
