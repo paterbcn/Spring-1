cde2c2d396dcd3e3c3e51825442d494dd0ef9732
Oriol Sastre <oriol.sastre@gmail.com>
2023-03-08 12:59:10 +0100

    entrega 1.5 ba

--- Entrega1.5.js
+++ Entrega1.5.js
@@ -44,7 +44,7 @@ function intervalFunc (msg){
 // N2 ejercicio 2
 //Crea una funció que llisti per la consola el contingut del directori d'usuari/ària de l'ordinador 
 // utilizant Node Child Processes.
-
+// https://www.geeksforgeeks.org/node-js-child-process/
 const userDir = process.env.userprofile
 fs.readdir(userDir,(err,files)=>{
 if (err){throw err};{
@@ -56,5 +56,50 @@ console.log(files)}})
 //Crea una altra funció que desencripti i descodifiqui els fitxers de l'apartat anterior tornant a generar una còpia de l'inicial.
 // Inclou un README amb instruccions per a l'execució de cada part.
 
+// https://www.geeksforgeeks.org/node-js-crypto-createcipheriv-method/
+
+function codFiles (nom){
+    const txt = fs.readFileSync(`./files/${nom}.txt`) 
+    fs.writeFile(`./files/${nom}base64.txt` ,txt.toString("base64"),
+        (err)=>{if (err){throw err}
+                    {console.log("base64 convertion done")}})
+    fs.writeFile(`./files/${nom}hex.txt` ,txt.toString('hex'),
+        (err)=>{if (err){throw err}
+                    {console.log("hex convertion done")}})
+                
+    }
+
+    const bf = require('node:buffer');
+    const fs = require("fs")
+    const crypto = require("crypto");
+    const { scryptSync } = require('node:crypto');
+    /*
+    
+    const algorithm = "aes-192-cbc"
+    const key = crypto.randomBytes(32);
+    const iv = crypto.randomBytes(16)
+    */
+    const key = scryptSync(pasww,"salt",24);
+        const iv = crypto.randomBytes(16);
+    const encrypt= (nom,)=>{
+        
+        pasww = "0000"
+        
+        const algorithm = "aes-192-cbc";
+        const readSteam = fs.createReadStream(`./files/${"nom"}.txt`);
+        readSteam.pipe(crypto.createCipheriv(algorithm,key,iv)).pipe(fs.createWriteStream(`./files/${nom}cripto`))}
+    
+    //encrypt("test")
+    
+    const desencrypt= (nom,)=>{
+        
+        pasww = "0000"
+        
+        
+        const algorithm = "aes-192-cbc";
+        const readSteam = fs.createReadStream(`./files/${"nom"}`);
+        readSteam.pipe(crypto.createDecipheriv(algorithm,key,iv)).pipe(fs.createWriteStream(`./files/${nom}.txt`))}
+    
+        desencrypt("testcripto")
 
 
